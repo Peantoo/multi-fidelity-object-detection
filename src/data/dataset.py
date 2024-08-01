00852268fa426240d8f68468f2fcba67df19b40f
Dustin Huffman <dusand@gmail.com>
2024-07-31 20:50:26 -0400

    Added initial classes for datasets and the vae model.

---
+++ src/data/dataset.py
@@ -0,0 +1,12 @@
+class MultiFidelityDataset:
+    def __init__(self):
+        # TODO: Implement dataset initialization
+        pass
+
+    def __len__(self):
+        # TODO: Implement dataset length
+        pass
+
+    def __getitem__(self, idx):
+        # TODO: Implement item retrieval
+        pass
---
+++ src/models/vae.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        # TODO: Implement VAE architecture
+
+    def forward(self, x):
+        # TODO: Implement forward pass
+        pass
